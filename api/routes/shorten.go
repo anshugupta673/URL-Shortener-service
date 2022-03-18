7f68ede07e11dbf0fcaffe99d6b1781469a1c0b5
anshugupta673 <alch.anshu@gmail.com>
2022-03-18 09:02:51 +0530

    rate limit update

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/anshugupta673/URL-Shortener-service/database"
 	"github.com/anshugupta673/URL-Shortener-service/helpers"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
 )
 
 type request struct { /* golang do not nderstand json on it's own so it has to do a lot of encoding and decoding with json, searilization bascially, so we need to tell what it's going to look like when we come across json when we receive a request */
@@ -30,10 +35,26 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//implement rate limiting
 	//...we check the IP of the user, and we check if IP is stored in the database, if IP is stored in the database then that means user has already used our service, if yes then devrement the number of rateRemaining by 1
+	//check if the IP address of the user has been already entered in our database if it's not there if the user is using the service for the first time... if user has already used the service (IP is stored in the database)
+	r2 := database.CreateClient(1) /* redis database client */
+	defer r2.Close()
+	val, err := r2.Get(databaseCtx, c.IP()).Result() /* IP: key, value: "" */
+	if err == redis.Nil {                            /* when i dont find the IP of the user in the database */
+		_ = r2.Set(database.Ctx, c.IP, os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	}
 
+	//rate limiting logic: create a db client, get the val associated with the key IP if we didnt find anything in the database then set for the IP address we define 30min as the time limit else {  }
 	//check if the input sent by the user if an actual URL
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP().Result())
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctxm c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "rate limit excedded", 
+		})
+		}
 	}
 
 	//also there's something that the user can do, that user can use local host 3000 as the URL to get shortened and then program can enter a infinite loop and we dont want that to happen(so we need some helper functions to hold that... so we check for domain error also we enforce https or ssl)
@@ -44,5 +65,5 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	//enforce https, SSL
-	body.URL = helpers.EnforceHTTP(body.URL) 
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
